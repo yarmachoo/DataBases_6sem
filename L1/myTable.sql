374a9606d776eef365326bff28ab66fa110ad873
Vero <veroni.rmk@gmail.com>
2025-02-10 14:54:52 +0300

    Implement task1 from L1

--- L1/myTable.sql
+++ L1/myTable.sql
@@ -4,3 +4,14 @@ CREATE TABLE MYTABLE (
 );
 
 SELECT * FROM MYTABLE;
+
+-- Реализовать анонимный блок для вставки 10 000 случайных значений
+DECLARE
+    counter NUMBER := 1;
+BEGIN
+    FOR i IN 1..10000 LOOP
+        INSERT INTO MYTABLE (ID, VAL)
+        VALUES (i, TRUNC(DBMS_RANDOM.VALUE(1,100)));
+    END LOOP;
+    COMMIT;
+END;
