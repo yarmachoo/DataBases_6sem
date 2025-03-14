d31feba850eeff6315119bd1c9ba06dda140271d
Vero <veroni.rmk@gmail.com>
2025-03-14 16:46:06 +0300

    Fix bugs in task 5 of 2 lab and add tests

--- L2/L2.sql
+++ L2/L2.sql
@@ -1,129 +1,118 @@
-CREATE TABLE STUDENTS (
-    ID NUMBER PRIMARY KEY,
-    "NAME" VARCHAR2(100),
-    "GROUP_ID" NUMBER
-);
-
-select * from STUDENTS;
-
-
 CREATE TABLE GROUPS (
-    ID NUMBER PRIMARY KEY,
-    "NAME" VARCHAR2 (100),
-    C_VAL NUMBER
+    GROUP_ID NUMBER NOT NULL,
+    GROUP_NAME VARCHAR2(20) NOT NULL,
+    C_VAL NUMBER DEFAULT 0 NOT NULL
 );
 
-SELECT * FROM GROUPS;
+CREATE TABLE STUDENTS (
+    STUDENT_ID NUMBER NOT NULL,
+    STUDENT_NAME VARCHAR2(20) NOT NULL,
+    GROUP_ID NUMBER NOT NULL
+);
 
-//2. Реализовать триггеры для таблиц 
-//задания 1 
-//1 - проверку целостности (проверка на уникальность полей ID),
-//2 - генерацию автоинкрементного ключа и 
-//3 - проверку уникальности для поля GROUP.NAME
+DROP TABLE GROUPS;
+DROP TABLE STUDENTS;
 
-//1
+///////////////////////////////////////////////////////
+//Task2
 
-create or replace trigger trigger_unique_group_id
-before insert on GROUPS
-for each ROW
-declare
-    v_count NUMBER;
-BEGIN
-    select count(*) INTO v_count from groups where ID=:New.ID;
 
-    if v_count >0 THEN
-        Raise_Application_Error(-20001, 'ERROR: ID of group should be unique!');
-    END IF;
-End;
-
-create or replace trigger trigger_unique_student_id
-before insert on STUDENTS
-for each ROW
-declare
-    v_count NUMBER;
-BEGIN
-    select count(*) INTO v_count from STUDENTS where ID=:New.ID;
-
-    if v_count >0 THEN
-        Raise_Application_Error(-20001, 'ERROR: ID of student should be unique!');
-    END IF;
-End;
-
-//2
-//последовательность для автоинкрементов
 CREATE SEQUENCE STUDENTS_SEQ START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE GROUPS_SEQ START WITH 1 INCREMENT BY 1;
 
-create or replace trigger trigger_auto_student_id
-before insert on STUDENTS
-for each ROW
-begin 
-    if :NEW.ID is NULL THEN
-        :NEW.ID:=STUDENTS_SEQ.NEXTVAL;
+DROP SEQUENCE STUDENTS_SEQ;
+DROP SEQUENCE GROUPS_SEQ;
+
+CREATE OR REPLACE TRIGGER trigger_auto_group_id
+BEFORE INSERT ON groups
+FOR EACH ROW
+BEGIN
+    IF :NEW.group_id IS NULL THEN
+        :NEW.group_id := GROUPS_SEQ.NEXTVAL;
     END IF;
 END;
 /
 
-create or replace trigger students_auto_stident_id
-before insert on GROUPS
-for each ROW
-begin
-    if :NEW.ID is NULL THEN
-     :New.ID:=GROUPS_SEQ.NEXTVAL;
-    END If;
-End;
-
-//3
-create or replace trigger trigger_unique_group_name
-before insert or update on GROUPS
-for each ROW
-declare
-    v_count NUMBER;
+CREATE OR REPLACE TRIGGER trigger_auto_student_id
+BEFORE INSERT ON students
+FOR EACH ROW
 BEGIN
-    select COUNT(*) into v_count
-    from GROUPS
-    WHERE LOWER("NAME")=LOWER(:NEW."NAME")
-    AND (ID <> :NEW.ID OR :NEW.ID IS NULL);
-
-    IF v_count >0 THEN
-        Raise_Application_Error(-20002,  'ERROR: name of group should be unique!');
+    IF :NEW.student_id IS NULL THEN
+   :NEW.student_id := STUDENTS_SEQ.NEXTVAL;
     END IF;
 END;
+/
 
 
-//check triggers
-INSERT INTO STUDENTS ("NAME", "GROUP_ID") VALUES ('John Doe', 1);
+CREATE OR REPLACE TRIGGER trigger_unique_group_id
+BEFORE INSERT OR UPDATE ON groups
+FOR EACH ROW
+DECLARE
+    v_count NUMBER;
+BEGIN
+    IF INSERTING THEN
+        SELECT COUNT(*) INTO v_count FROM groups WHERE group_id = :NEW.group_id;
+        
+        IF v_count > 0 THEN
+            RAISE_APPLICATION_ERROR(-20001, 'ERROR: ID of group should be unique!');
+        END IF;
+    END IF;
+    
+    IF UPDATING THEN
+        NULL; 
+    END IF;
+END;
+/
 
-select * from STUDENTS;
-INSERT INTO STUDENTS ("NAME", "GROUP_ID") VALUES ('Doe', 5);
-INSERT INTO STUDENTS (ID, "NAME", "GROUP_ID") VALUES (1, 'Doe', 1);
+CREATE OR REPLACE TRIGGER trigger_unique_student_id
+BEFORE INSERT ON students
+FOR EACH ROW
+DECLARE
+    PRAGMA AUTONOMOUS_TRANSACTION;  
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM students WHERE student_id = :NEW.student_id;
+    
+    IF v_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'ERROR: ID of student should be unique!');
+    END IF;
+END;
+/
 
-INSERT INTO GROUPS ("NAME", C_VAL) VALUES ('Group A', 5);
-INSERT INTO GROUPS ("NAME", C_VAL) VALUES ('Group B', 5);
-select * from GROUPS;
+CREATE OR REPLACE TRIGGER trigger_unique_group_name
+AFTER INSERT OR UPDATE ON groups
+FOR EACH ROW
+DECLARE
+    PRAGMA AUTONOMOUS_TRANSACTION;
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_count 
+    FROM groups 
+    WHERE LOWER(group_name) = LOWER(:NEW.group_name)
+    AND group_id <> :NEW.group_id; 
+    
+    IF v_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20002,  'ERROR: name of group should be unique!');
+    END IF;
+END;
+/
 
-INSERT INTO GROUPS (ID, "NAME", C_VAL) VALUES (5, 'Group C', 5);
-
-INSERT INTO GROUPS ("NAME", C_VAL) VALUES ('Group B', 5);
-
-
-//////////////////////////////////////////////////////////////////
-
-//cascade delete:
+///////////////////////////////////////////////////////
+//Task 3
 
 CREATE OR REPLACE PACKAGE global_variables AS
     is_group_delete_cascade BOOLEAN := FALSE;
 END global_variables;
 /
 
-create or replace trigger trigger_delete_group_cascade
-before delete on GROUPS
-for each ROW
+CREATE OR REPLACE TRIGGER tl_delete_group_cascade
+BEFORE DELETE ON groups
+FOR EACH ROW
 BEGIN
-    global_variables.is_group_delete_cascade:=true;
-
-    delete from STUDENTS
-    WHERE "GROUP_ID" = :OLD.ID;
+    global_variables.is_group_delete_cascade := TRUE;
+    
+    DELETE FROM students
+    WHERE group_id = :OLD.group_id;  
 
     global_variables.is_group_delete_cascade := FALSE;
 EXCEPTION
@@ -131,170 +120,282 @@ EXCEPTION
         global_variables.is_group_delete_cascade := FALSE;
         RAISE;
 END;
+/
 
-create or replace trigger trigger_chack_group_exists
-before insert or update on STUDENTS
-for each ROW
+CREATE OR REPLACE TRIGGER trigger_check_group_exists
+BEFORE INSERT OR UPDATE ON students
+FOR EACH ROW
 DECLARE
-v_count NUMBER;
+    v_count NUMBER;
 BEGIN
-    SELECT COUNT(*) INTO v_count
-    from GROUPS
-    WHERE ID = :New."GROUP_ID";
+    SELECT COUNT(*) INTO v_count 
+    FROM groups 
+    WHERE group_id = :NEW.group_id;  
 
     IF v_count = 0 THEN
-        Raise_Application_Error(-20000, 'ERROR: Group with ID' || :NEW."GROUP_ID" || ' is not exists.');
+        RAISE_APPLICATION_ERROR(-20000, 'ERROR: Group with ID' || :NEW."GROUP_ID" || ' is not exists.');
     END IF;
-
 END;
+/
 
-create or replace trigger trigger_prevent_group_id_update
-before update of "GROUP_ID" on STUDENTS
+CREATE OR REPLACE TRIGGER prevent_group_id_update
+BEFORE UPDATE OF group_id ON groups
 FOR EACH ROW
-DECLARE 
-    students_exists NUMBER;
+DECLARE
+    students_exist NUMBER;
 BEGIN
-    SELECT COUNT(*) INTO students_exists
-    FROM STUDENTS
-    WHERE "GROUP_ID" = :OLD."GROUP_ID";
+    SELECT COUNT(*) INTO students_exist
+    FROM students
+    WHERE group_id = :OLD.group_id;
 
-    IF students_exists = 0 THEN
-        Raise_Application_Error(-20000, 'ERROR: this group has students. You can not update ID.');
+    IF students_exist > 0 THEN
+        RAISE_APPLICATION_ERROR(-20000, 'ERROR: this group has students. You can not update ID.');
     END IF;
 END;
+/
+///////////////////////////////////////////////////////
+//Task 4
 
-select * from GROUPS
-select * from STUDENTS
+CREATE OR REPLACE PACKAGE student_ctx AS
+    //array for group_id
+    TYPE t_group_name_table IS TABLE OF VARCHAR2(255) INDEX BY BINARY_INTEGER;
+    g_group_names t_group_name_table;
+    PROCEDURE load_group_name(p_group_id NUMBER, p_group_name VARCHAR2);
+END student_ctx;
+/
 
-DELETE FROM GROUPS WHERE "NAME"='Group A'
+CREATE OR REPLACE PACKAGE BODY student_ctx AS
+    PROCEDURE load_group_name(p_group_id NUMBER, p_group_name VARCHAR2) IS
+    BEGIN
+        g_group_names(p_group_id) := p_group_name;
+    END load_group_name;
+END student_ctx;
+/
 
+CREATE OR REPLACE TRIGGER cache_group_on_insert
+AFTER INSERT OR UPDATE ON groups
+FOR EACH ROW
+BEGIN
+    student_ctx.load_group_name(:NEW.group_id, :NEW.group_name);
+END;
+/
 
-////////////////////////////////////////////////////
-//task4
-CREATE TABLE STUDENTS_LOGS (
+CREATE TABLE students_logs (
     LOG_ID NUMBER PRIMARY KEY,
-    ACTION_TYPE VARCHAR2(10),     
-    STUDENT_ID NUMBER,            
-    STUDENT_NAME VARCHAR2(100),    
-    GROUP_ID NUMBER,              
-    ACTION_TIMESTAMP TIMESTAMP,    
-    ACTION_BY VARCHAR2(100)       
+    ACTION_TYPE VARCHAR2(10),
+    OLD_ID NUMBER,
+    NEW_ID NUMBER,
+    OLD_NAME VARCHAR2(255),
+    NEW_NAME VARCHAR2(255),
+    OLD_GROUP_ID NUMBER,
+    NEW_GROUP_ID NUMBER,
+    OLD_GROUP_NAME VARCHAR2(255),
+    NEW_GROUP_NAME VARCHAR2(255),
+    ACTION_TIME TIMESTAMP
 );
+/
 
-CREATE OR REPLACE TRIGGER trigger_insert_student_log
-AFTER INSERT ON STUDENTS
+drop table students_logs;
+
+CREATE SEQUENCE STUDENTS_LOGS_SEQ START WITH 1;
+/
+DROP SEQUENCE STUDENTS_LOGS_SEQ;
+
+
+CREATE OR REPLACE TRIGGER log_student_changes
+AFTER INSERT OR UPDATE OR DELETE ON students
 FOR EACH ROW
 BEGIN
-    INSERT INTO STUDENTS_LOGS (LOG_ID, ACTION_TYPE, STUDENT_ID, STUDENT_NAME, GROUP_ID, ACTION_TIMESTAMP, ACTION_BY)
-    VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'INSERT', :NEW.ID, :NEW."NAME", :NEW."GROUP_ID", SYSTIMESTAMP, USER);
+    IF INSERTING THEN
+        INSERT INTO students_logs (LOG_ID, ACTION_TYPE, NEW_ID, NEW_NAME, NEW_GROUP_ID, NEW_GROUP_NAME, ACTION_TIME)
+        VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'INSERT', :NEW.student_id, :NEW.student_name, :NEW.group_id, student_ctx.g_group_names(:NEW.group_id), CURRENT_TIMESTAMP);
+    ELSIF UPDATING THEN
+        INSERT INTO students_logs (LOG_ID, ACTION_TYPE, OLD_ID, NEW_ID, OLD_NAME, NEW_NAME, OLD_GROUP_ID, OLD_GROUP_NAME, NEW_GROUP_ID, NEW_GROUP_NAME, ACTION_TIME)
+        VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'UPDATE', :OLD.student_id, :NEW.student_id, :OLD.student_name, :NEW.student_name, :OLD.group_id, student_ctx.g_group_names(:OLD.group_id), :NEW.group_id, student_ctx.g_group_names(:NEW.group_id), CURRENT_TIMESTAMP);
+    ELSIF DELETING THEN
+        INSERT INTO students_logs (LOG_ID, ACTION_TYPE, OLD_ID, OLD_NAME, OLD_GROUP_ID, OLD_GROUP_NAME, ACTION_TIME)
+        VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'DELETE', :OLD.student_id, :OLD.student_name, :OLD.group_id, student_ctx.g_group_names(:OLD.group_id), CURRENT_TIMESTAMP);
+    END IF;
 END;
 /
 
-CREATE OR REPLACE TRIGGER trigger_update_student_log
-AFTER UPDATE ON STUDENTS
-FOR EACH ROW
-BEGIN
-    INSERT INTO STUDENTS_LOGS (LOG_ID, ACTION_TYPE, STUDENT_ID, STUDENT_NAME, GROUP_ID, ACTION_TIMESTAMP, ACTION_BY)
-    VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'UPDATE', :NEW.ID, :NEW."NAME", :NEW."GROUP_ID", SYSTIMESTAMP, USER);
-END;
-/
 
-CREATE OR REPLACE TRIGGER trigger_delete_student_log
-AFTER DELETE ON STUDENTS
-FOR EACH ROW
-BEGIN
-    INSERT INTO STUDENTS_LOGS (LOG_ID, ACTION_TYPE, STUDENT_ID, STUDENT_NAME, GROUP_ID, ACTION_TIMESTAMP, ACTION_BY)
-    VALUES (STUDENTS_LOGS_SEQ.NEXTVAL, 'DELETE', :OLD.ID, :OLD."NAME", :OLD."GROUP_ID", SYSTIMESTAMP, USER);
-END;
-/
+///////////////////////////////////////////////////////
+//TASK 5
 
-CREATE SEQUENCE STUDENTS_LOGS_SEQ
-START WITH 1 INCREMENT BY 1;
-
-INSERT INTO STUDENTS ("NAME", "GROUP_ID") VALUES ('John Doe', 5);
-UPDATE STUDENTS SET "NAME" = 'John Smith' WHERE "NAME" = 'John Doe';
-DELETE FROM STUDENTS WHERE "NAME" = 'John Smith';
-SELECT * FROM STUDENTS_LOGS;
-
-/////////////////////////////////////////////////////////
-//Task 5
- 
- create or replace procedure restore_students_from_logs(
+CREATE OR REPLACE PROCEDURE restore_students_from_logs(
     p_time TIMESTAMP DEFAULT NULL,
     p_offset INTERVAL DAY TO SECOND DEFAULT NULL
- ) IS
+) IS
     v_restore_time TIMESTAMP;
     v_group_exists NUMBER;
     v_student_exists NUMBER;
-begin
-    if p_time is not null THEN
-        v_restore_time:=p_time;
-    ELSIF p_offset is not null THEN
-        v_restore_time:=SYSTIMESTAMP - p_offset;
-    ELSE 
-        Raise_Application_Error(-20000, 'You need to send p_time or p_offset');
+    v_count_deleted NUMBER := 0;
+BEGIN
+    IF p_time IS NOT NULL THEN
+        v_restore_time := p_time;
+    ELSIF p_offset IS NOT NULL THEN
+        v_restore_time := CURRENT_TIMESTAMP - p_offset;
+    ELSE
+        RAISE_APPLICATION_ERROR(-20000, 'You need to send p_time or p_offset');
     END IF;
 
     DBMS_OUTPUT.PUT_LINE('Restore data from ' || TO_CHAR(v_restore_time, 'DD-MM-YYYY HH24:MI:SS'));
+
+    SELECT COUNT(*) INTO v_count_deleted
+    FROM students_logs
+    WHERE action_time >= v_restore_time
+      AND action_type = 'DELETE';
+
+    IF v_count_deleted = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('There is no DELETE records in students_logs. Restoring is not needed.');
+        RETURN;
+    END IF;
+
     FOR record IN (
-        SELECT DISTINCT STUDENT_ID, STUDENT_NAME, GROUP_ID
-        FROM STUDENTS_LOGS
-        WHERE ACTION_TIMESTAMP >= v_restore_time
-          AND ACTION_TYPE = 'DELETE'
+        SELECT DISTINCT old_group_id, old_group_name
+        FROM students_logs
+        WHERE action_time >= v_restore_time
+          AND old_group_id IS NOT NULL
+          AND old_group_name IS NOT NULL
     ) LOOP
         SELECT COUNT(*) INTO v_group_exists
-        FROM GROUPS
-        WHERE ID = record.GROUP_ID;
+        FROM groups
+        WHERE group_id = record.old_group_id;
 
         IF v_group_exists = 0 THEN
-            INSERT INTO GROUPS (ID, "NAME")
-            VALUES (record.GROUP_ID, 'Group ' || record.GROUP_ID);
-            DBMS_OUTPUT.PUT_LINE('The group with ID ' || record.GROUP_ID || ' is restored');
+            INSERT INTO groups (group_id, group_name)
+            VALUES (record.old_group_id, record.old_group_name);
+            DBMS_OUTPUT.PUT_LINE('The group with ID ' || record.old_group_id || ' is restored');
         END IF;
     END LOOP;
 
     FOR record IN (
-        SELECT * FROM STUDENTS_LOGS
-        WHERE ACTION_TIMESTAMP >= v_restore_time
-          AND ACTION_TYPE = 'DELETE'
-        ORDER BY ACTION_TIMESTAMP DESC
+        SELECT * FROM students_logs
+        WHERE action_time >= v_restore_time
+          AND action_type = 'DELETE'
+        ORDER BY action_time DESC
     ) LOOP
         SELECT COUNT(*) INTO v_student_exists
-        FROM STUDENTS
-        WHERE ID = record.STUDENT_ID;
+        FROM students
+        WHERE student_id = record.old_id;
 
         IF v_student_exists = 0 THEN
-            INSERT INTO STUDENTS (ID, "NAME", GROUP_ID)
-            VALUES (record.STUDENT_ID, record.STUDENT_NAME, record.GROUP_ID);
-            DBMS_OUTPUT.PUT_LINE('Student with ID ' || record.STUDENT_ID || ' is restored');
-        ELSE
-            DBMS_OUTPUT.PUT_LINE('Student with ID ' || record.STUDENT_ID || ' already exists, skip.');
+        EXECUTE IMMEDIATE 'ALTER TRIGGER trigger_check_group_exists DISABLE';
+            INSERT INTO students (student_id, student_name, group_id)
+            VALUES (record.old_id, record.old_name, record.old_group_id);
+            DBMS_OUTPUT.PUT_LINE('Student with ID ' || record.old_id || ' is restored');
+            EXECUTE IMMEDIATE 'ALTER TRIGGER trigger_check_group_exists ENABLE';
+        ELSE                     
+            DBMS_OUTPUT.PUT_LINE('Student with ID ' || record.old_id || ' already exists.');
         END IF;
     END LOOP;
 
     COMMIT;
-    DBMS_OUTPUT.PUT_LINE('Restore was end.');
+    DBMS_OUTPUT.PUT_LINE('Restore is end.');
 
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLERRM);
         ROLLBACK;
 END;
 /
-INSERT INTO STUDENTS ("NAME", "GROUP_ID") VALUES ('Veronika', 5);
-select * from STUDENTS
-select * from GROUPS
-select * from STUDENTS_
 
-delete from groups where ID = 5;
-delete from students where ID = 4;
+///////////////////////////////////////////////////////
+//TASK 6
+
+CREATE OR REPLACE TRIGGER trigger_update_c_val_on_insert
+BEFORE INSERT ON students
+FOR EACH ROW
+BEGIN
+    UPDATE groups
+    SET c_val = c_val + 1
+    WHERE group_id = :NEW.group_id;
+END;
+/
+
+CREATE OR REPLACE TRIGGER trigger_update_c_val_on_delete
+BEFORE DELETE ON students
+FOR EACH ROW
+BEGIN
+    IF NOT global_variables.is_group_delete_cascade THEN
+        UPDATE groups
+        SET c_val = c_val - 1
+        WHERE group_id = :OLD.group_id;
+    END IF;
+END;
+/
+
+CREATE OR REPLACE TRIGGER trigger_update_c_val_on_update
+BEFORE UPDATE OF group_id ON students
+FOR EACH ROW
+BEGIN
+    IF :OLD.group_id != :NEW.group_id THEN
+        UPDATE groups
+        SET c_val = c_val - 1
+        WHERE group_id = :OLD.group_id;
+        UPDATE groups
+        SET c_val = c_val + 1
+        WHERE group_id = :NEW.group_id;
+    END IF;
+END;
+/
+
+///////////////////////////////////////////////////////////////////
+//TEST 1 TASK
+
+SELECT * FROM students;
+SELECT * FROM groups;
+
+//TEST 2 TASK 
+
+INSERT INTO GROUPS ("GROUP_NAME") VALUES ('Group D');
+INSERT INTO STUDENTS ("STUDENT_NAME", "GROUP_ID") VALUES ('John Doe', 4);
+INSERT INTO GROUPS ("GROUP_ID", "GROUP_NAME") VALUES (2, 'Group B');
+INSERT INTO GROUPS ("GROUP_ID", "GROUP_NAME") VALUES (3, 'Group C');
+DELETE FROM GROUPS WHERE GROUP_ID = 2;
+
+//TEST 3 Task
+
+SELECT * FROM students;
+SELECT * FROM groups;
+
+
+INSERT INTO STUDENTS ("STUDENT_NAME", "GROUP_ID") VALUES ('Veronika', 2);
+DELETE FROM GROUPS where GROUP_ID=3;
+
+//TEST 4 Task
+
+SELECT sessiontimezone, dbtimezone FROM dual;
+SELECT CURRENT_TIMESTAMP FROM dual;
+Delete from students where student_id=3;
+
+SELECT * FROM students_LOGs;
+
+//TEST 5 TASK
+
+SELECT * FROM students;
+SELECT * FROM groups;
+SELECT * FROM students_LOGs;
 
 BEGIN
-    restore_students_from_logs(TIMESTAMP '2025-03-10 17:00:00', NULL);
+    restore_students_from_logs(NULL, INTERVAL '3' MINUTE);
 END;
 /
 
 BEGIN
-    restore_students_from_logs(NULL, INTERVAL '10' MINUTE);
+    restore_students_from_logs(NULL, TO_DSINTERVAL('0 00:08:00'));
 END;
 /
+
+BEGIN
+    restore_students_from_logs(TIMESTAMP '2025-03-14 16:13:23', NULL);
+END;
+/
+
+//TEST 6 TASK
+
+SELECT * FROM students;
+SELECT * FROM groups;
+
+UPDATE STUDENTS SET group_id=2 WHERE student_id = 2;
